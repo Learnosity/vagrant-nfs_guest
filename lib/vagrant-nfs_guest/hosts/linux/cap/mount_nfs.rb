680af7a3440f81d68a58550f366ca150be36343f
Stella Lie <ask@stellalie.com>
2014-07-29 23:28:02 +1000

    Some hack to allow this vagrant plugin to run on my Ubuntu machine

---
+++ lib/vagrant-nfs_guest/hosts/linux/cap/mount_nfs.rb
@@ -0,0 +1,31 @@
+module VagrantPlugins
+  module SyncedFolderNFSGuest
+    module HostLinux
+      module Cap
+        class MountNFS
+
+          def self.nfs_mount(environment, ui, id, ips, folders)
+            folders.each do |name, opts|
+              if opts[:type] != :nfs_guest
+                next
+              end
+
+              ips.each do |ip|
+                ui.detail(I18n.t("vagrant.actions.vm.share_folders.mounting_entry",
+                                 guestpath: opts[:guestpath],
+                                 hostpath: opts[:hostpath]))
+
+                system("mkdir -p #{opts[:hostpath]}")
+                mount_command = "sudo mount -t nfs -o noatime '#{ip}:#{opts[:guestpath]}' '#{opts[:hostpath]}'"
+                if system(mount_command)
+                  break
+                end
+              end
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
