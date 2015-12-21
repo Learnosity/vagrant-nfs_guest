1b3e2a5762d7aed64c6f4adf1c2b2752b9833657
Alan Garfield <alan@fromorbit.com>
2015-12-22 09:07:32 +1100

    [FIX] typo in parallels provider plugin

--- lib/vagrant-nfs_guest/providers/parallels/cap/nfs_settings.rb
+++ lib/vagrant-nfs_guest/providers/parallels/cap/nfs_settings.rb
@@ -4,8 +4,8 @@ module VagrantPlugins
       module Cap
 
         def self.nfs_settings(machine)
-          host_ip  = @machine.provider.driver.read_shared_interface[:ip]
-          machine_ip = @machine.provider.driver.read_guest_ip
+          host_ip  = machine.provider.driver.read_shared_interface[:ip]
+          machine_ip = machine.provider.driver.read_guest_ip
 
           raise Vagrant::Errors::NFSNoHostonlyNetwork if !host_ip || !machine_ip
 
